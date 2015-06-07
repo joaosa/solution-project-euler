56ef8a087a1440148f41adcafa05bd9a4b143864
Joao Andrade <jjadonotenter@gmail.com>
2015-06-07 19:14:35 +0100

    Fix function name

--- clojure/1.clj
+++ clojure/1.clj
@@ -1,8 +1,8 @@
 (defn multiple-3-or-5? [a] (or (= 0 (mod a 3)) (= 0 (mod a 5))))
 
-(defn solution [limit]
+(defn sum-multiples [limit]
   (let [number-range (range 1 limit)
         multiples (filter multiple-3-or-5? number-range)]
     (reduce + multiples)))
 
-(print (solution 1000))
+(print (sum-multiples 1000))
