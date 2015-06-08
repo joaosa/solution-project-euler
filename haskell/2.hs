c985ca00eb351e84d38c8ea0f361d6ed716693dc
Joao Andrade <jjadonotenter@gmail.com>
2015-06-08 01:08:28 +0100

    Use a more reliable haskell solution

--- haskell/2.hs
+++ haskell/2.hs
@@ -1,15 +1,12 @@
 -- start with 1 and 2
-fib :: Integer -> Integer
-fib 0 = 0
-fib 1 = 1
-fib 2 = 2
-fib n = fib (n-1) + fib (n-2)
-
 fibs :: [Integer]
-fibs = map fib [1..]
+fibs = 1 : 2 : zipWith (+) fibs (tail fibs)
 
-evenFibUnder4M :: [Integer]
-evenFibUnder4M = filter (\n -> (n < 4000000) && (even n)) fibs
+fibUnder4M :: [Integer]
+fibUnder4M = takeWhile (< 4000000) fibs
+
+evenFib :: [Integer]
+evenFib = filter even fibUnder4M
 
 main :: IO ()
-main = do print (sum evenFibUnder4M)
+main = do print (sum evenFib)
